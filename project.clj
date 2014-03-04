03782e4a698cf77f7dfeb1fb53e2202913965d85
we <we@we-desktop.(none)>
2014-03-04 09:43:42 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,37 @@
+(defproject gh "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/algo.generic "0.1.1"]
+                 [clj-time "0.6.0"]
+                 [org.clojure/data.priority-map "0.0.4"]
+                 [org.clojure/data.json "0.2.4"]
+                 [prismatic/plumbing "0.2.1"]
+                 [incanter/incanter-core "1.5.4"]
+                 [incanter/incanter-charts "1.5.4"]
+                 [incanter/incanter-zoo "1.5.4"]
+                 ;; [com.jmeeks/clj-web-crawler "0.1.0-SNAPSHOT"]
+                 ;; [org.clojars.kjw/commons-httpclient "3.1"]
+                 [net.mikera/core.matrix "0.20.0"]
+                 ;; [org.clojure/data.csv "0.1.2"]
+                 ;; [clojure-csv/clojure-csv "2.0.1"]
+                 ;; [com.nuroko/nurokit "0.0.3"]
+                 ;; [org.clojure/algo.generic "0.1.1"]
+                 ;; [org.clojure/data.json "0.2.3"]
+                 ;; [com.keminglabs/c2 "0.2.3"]
+                 ;; [hiccup "1.0.4"]
+                 ;; [cc.artifice/clj-ml "0.6.0-SNAPSHOT"]
+                 ;; [org.clojure/tools.trace "0.7.6"]
+                 ;; [clj-liblinear "0.1.0"]
+                 ;; [org.clojure/data.generators "0.1.2"]
+                 ;; [svm-clj "0.1.3"]
+                 ;; [clatrix "0.3.0"]
+                 ;; [org.apache.commons/commons-math3 "3.2"]
+                 ;; [net.sf.jung/jung-api "2.0.1"]
+                 ;; [net.sf.jung/jung-graph-impl "2.0.1"]
+                 ;; [net.sf.jung/jung-algorithms "2.0.1"]
+                 ;; [net.sf.jung/jung-algorithms "2.0.1"]
+                 ;; [net.sf.jung/jung-visualization "2.0.1"]
+                 ])
---
+++ src/gh/core.clj
@@ -0,0 +1,196 @@
+(ns gh.core
+  (:use [plumbing.core :only [fnk for-map defnk]]
+        [clojure.pprint]
+        [clojure.data.priority-map]
+        [clojure.repl]
+        [clj-time.periodic]
+        [incanter core stats charts io zoo]
+        [clojure.algo.generic.functor :only [fmap]])
+  (:require [plumbing.graph :as graph]
+            [plumbing.fnk.pfnk :as pfnk]
+            [clojure.java [io :as io] [shell :as shell]]
+            [clojure.data.json :as json]
+            [clj-time.core :as t]
+            [clojure.stacktrace :as st])
+  (:import java.util.zip.GZIPInputStream
+           java.io.BufferedInputStream
+           java.io.BufferedOutputStream
+           java.io.FileOutputStream           
+           java.io.File
+           java.lang.Math
+           java.io.InputStreamReader 
+           java.io.BufferedReader))
+
+(def ^:dynamic cache-path
+  "the path for caching results of computations"
+  "/home/we/workspace/data/gh/")
+
+(defn memoize-fnk [f]
+  (pfnk/fn->fnk (memoize f) (pfnk/io-schemata f)))
+
+(defrecord Ymdh [year month day hour])
+
+(defn
+  ^{:doc "Construct the filaname of github archive data corresponding to given year, month, day and hour."
+    :test (assert (= "2012-04-11-15.json.gz"
+                     (construct-filename (Ymdh. 2012 04 11 15))))}
+  construct-filename [ymdh]
+  (str (:year ymdh)
+       "-" (format "%02d" (:month ymdh))
+       "-" (format "%02d" (:day ymdh))
+       "-" (:hour ymdh)
+       ".json.gz"))
+
+(defn download-data [ymdh]
+  (let [filename (construct-filename ymdh)
+        path (str "/home/we/workspace/data/github-archive/"
+                  filename)]
+    (if (not (.exists (File. path)))
+      (let [url (str "http://data.githubarchive.org/"
+                     filename)]
+        (println ["downloading" url])
+        (shell/sh "wget" "-q" url "-O" path)))
+    path))
+
+(defn gunzip
+  [fi fo]
+  (with-open [i (io/reader
+                 (java.util.zip.GZIPInputStream.
+                  (io/input-stream fi)))
+              o (java.io.PrintWriter. (io/writer fo))]
+    (doseq [l (line-seq i)]
+      (.println o l))))
+
+(defn construct-data-reader [ymdh]
+  
+  )
+
+(defn read-as-json [ymdh]
+  (with-open [reader (-> ymdh
+                         download-data
+                         io/input-stream
+                         GZIPInputStream.
+                         io/reader)]
+    (json/read-str (str "["
+                        (clojure.string/replace
+                         (apply str (line-seq reader))
+                         "#}{" "#},{")
+                        "]")
+                   :key-fn keyword)))
+
+(defn analyse [ymdh]
+  (->> ymdh
+       read-as-json
+       (map (comp :language :repository))
+       frequencies
+       (into (priority-map))))
+
+(defn date-time-to-ymdh [dt]
+  (Ymdh. (t/year dt)
+           (t/month dt)
+           (t/day dt)
+           (t/hour dt)))
+
+(defn construct-ymdhs-seq
+  [beginning-date-time]
+  (map date-time-to-ymdh
+       (periodic-seq beginning-date-time
+                     org.joda.time.Hours/ONE)))
+
+(def all-relevant-date-times
+  (periodic-seq ;(t/date-time 2011 2 12)
+   (t/date-time 2012 3 12)
+   org.joda.time.Hours/ONE))
+
+(comment
+  (let [d (->> (construct-ymdhs-seq (t/date-time 2013))
+               (take 1200)
+               (pmap (fn [ymdh]
+                       (into (select-keys (analyse ymdh)
+                                          ["Java" "JavaScript"])
+                             ymdh)))
+               to-dataset)]
+    (view
+     (add-lines
+      (xy-plot (range (nrow d))
+               ($  "JavaScript" d))
+      (range (nrow d))
+      ($ "Java" d)))))
+
+(comment
+  (let [d (->> all-relevant-date-times
+               (filter #(and (= 0 (t/hour %))
+                             (= 1 (t/day-of-week %))))
+               (map date-time-to-ymdh)
+               (take 100)
+               (pmap (fn [ymdh]
+                       (into (select-keys (analyse ymdh)
+                                          ["Java" "JavaScript" "Clojure"])
+                             ymdh)))
+               to-dataset)]
+    (view
+          (add-lines
+           (add-lines
+            (xy-plot (range (nrow d))
+                     (map log ($  "JavaScript" d)))
+            (range (nrow d))
+            (map log ($ "Clojure" d)))
+           (range (nrow d))
+           (map log ($ "Java" d))))))
+
+(comment
+  (->> all-relevant-date-times
+       (filter #(and (= 0 (t/hour %))
+                     (= 1 (t/day-of-week %))))
+       (map date-time-to-ymdh)
+       (take 10)
+       (mapcat read-as-json)
+       (map :repository)
+       (filter :url)
+       (group-by :url)
+       (map (comp last second))
+       (map #(select-keys % [:watchers :forks :has_wiki :has_issues :language]))
+       to-dataset
+       ($where {:language {:$in #{"Perl" "Clojure" "Scala" "C"}}})
+       (add-derived-column :log-forks [:forks] (comp log inc))
+       (add-derived-column :log-watchers [:watchers] (comp log inc))
+       (scatter-plot :log-forks :log-watchers
+                     :group-by :language
+                     :data)
+       view)
+)
+
+
+(comment
+  (let [events-set #{"WatchEvent" "PushEvent"}]    
+    (->> all-relevant-date-times
+         (filter #(and (= 0 (t/hour %))
+                       (= 1 (t/day-of-week %))))
+         (map date-time-to-ymdh)
+         (take 30)
+         (mapcat read-as-json)
+         (map #(into (select-keys % [:type])
+                     (select-keys (:repository %) [:language])))
+         (filter (comp events-set
+                       :type))
+         frequencies
+         (map (fn [[val count]]
+                (into val
+                      {:count count})))
+         (group-by :language)
+         (map (fn [[language event-counts]]
+                (into {:language language}
+                      (map (juxt (comp keyword
+                                       :type)
+                                 :count)
+                           event-counts))))
+         (filter #(and (= (inc (count events-set))
+                          (count %))
+                       (< 10 (apply min (vals (dissoc % :language))))))
+         (map #(fmap (fn [val]
+                       (if (number? val) (log val) val))
+                     %))
+         to-dataset
+         (scatter-plot :WatchEvent :PushEvent
+                       :data)
+         view)))
